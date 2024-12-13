d06a142587b0b2de44f04a017767b29f785fa209
JessDuff <jess.tasstic@gmail.com>
2024-12-13 08:56:05 -0400

    nombre medicamento

--- onlyabue/components/recordatorios/notificacionesService.js
+++ onlyabue/components/recordatorios/notificacionesService.js
@@ -3,7 +3,7 @@ import * as Notifications from 'expo-notifications';
 import { firestore } from '../../services/firebaseConfig';
 
 export async function primerRecordatorio(medicamentoData) {
-  const { usuarioId, medicamentoId, intervalo, horaInicial, cantidad  } = medicamentoData;
+  const { usuarioId, medicamentoId, intervalo, horaInicial, cantidad, nombre  } = medicamentoData;
 
   const primeraNotificacion = new Date(horaInicial);
 
@@ -29,7 +29,7 @@ export async function primerRecordatorio(medicamentoData) {
   // await Notifications.cancelAllScheduledNotificationsAsync();
   await Notifications.scheduleNotificationAsync({
     content: {
-      title: "¡Es hora de tomar tu medicamento!",
+      title: `¡Es hora de tomar ${nombre}!`,
       body: "Es momento de tomar tu dosis. ¿Lo has tomado?",
       data: { medicamentoId, usuarioId }
     },
@@ -45,7 +45,7 @@ export async function primerRecordatorio(medicamentoData) {
   console.log("First notification scheduled for:", primeraNotificacion);
   let nextNotificationTime = new Date(primeraNotificacion);
   for (let i = 1; i < cantidad; i++) {
-    nextNotificationTime = new Date(nextNotificationTime.getTime() + intervalo * 60 * 1000); // Add interval in milliseconds
+    nextNotificationTime = new Date(nextNotificationTime.getTime() + intervalo * 60 * 1000);//minutos -> cambiar a horas
 
     await Notifications.scheduleNotificationAsync({
       content: {
